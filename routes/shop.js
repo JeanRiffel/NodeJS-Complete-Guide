3890347d7f3d9ba4e23d59cc0b90209926bb7178
jeanriffel <jeanriffel1@gmail.com>
2020-02-07 22:37:00 -0300

    adding new files

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,20 @@ const path = require('path');
 const express = require('express');
 const router =  express.Router();
 const rootDir = require('../util/path');
-
-const adminData = require('./admin')
+const adminData = require('./admin');
 
 router.get('/', (req, res, next)=>{
     console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;  
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'shop',
+        path: '/',
+        hashProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+        }     
+    );
 });
 
 module.exports = router;
